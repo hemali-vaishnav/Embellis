12d5413b309e2ecba47289398c45cce511269567
Hemali Vaishnav <hemalivaishnav11@gmail.com>
2026-05-08 23:27:48 +0530

    add .env

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,23 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({ 
-  plugins: [
-     tailwindcss(),
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    define: {
+      'process.env.REACT_APP_ENV': JSON.stringify(env.REACT_APP_ENV),
+      'process.env.REACT_APP_DEV_URL': JSON.stringify(env.REACT_APP_DEV_URL),
+      'process.env.REACT_APP_STAGING_URL': JSON.stringify(env.REACT_APP_STAGING_URL),
+      'process.env.REACT_APP_PROD_URL': JSON.stringify(env.REACT_APP_PROD_URL),
+    },
+    plugins: [
+      tailwindcss(),
+      react(),
+      babel({ presets: [reactCompilerPreset()] })
+    ],
+  }
 })
